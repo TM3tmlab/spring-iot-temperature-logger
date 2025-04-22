79ac31070a53955312451fd5f61b7365340bb166
TM3tmlab <7882295+TM3tmlab@users.noreply.github.com>
2025-04-22 22:07:16 +0900

    fix format

--- workspace/setupcheck/Main.java
+++ workspace/setupcheck/Main.java
@@ -3,7 +3,7 @@
  * 環境構築後実行される Java ファイル
  */
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Java 環境が正常に導入されました");
-  }
+    public static void main(String[] args) {
+        System.out.println("Java 環境が正常に導入されました");
+    }
 }
