616e65fd291d14499ac5dd6ecad63484d7a51252
TM3tmlab <7882295+TM3tmlab@users.noreply.github.com>
2025-08-08 13:09:52 +0900

    Reapply "add spring project"
    This reverts commit af56a1e3fb0ddb3d422e87cd2f972d02ea24f70a.

---
+++ src/test/java/human/college/it/iotrobot/IotrobotApplicationTests.java
@@ -0,0 +1,12 @@
+package human.college.it.iotrobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotrobotApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
