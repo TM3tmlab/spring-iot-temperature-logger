e32a10bd519868e09148ef8112e20689d1d03027
TM3tmlab <7882295+TM3tmlab@users.noreply.github.com>
2025-08-08 13:04:11 +0900

    add spring project

---
+++ src/main/java/human/college/it/iotrobot/IotrobotApplication.java
@@ -0,0 +1,13 @@
+package human.college.it.iotrobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotrobotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IotrobotApplication.class, args);
+    }
+
+}
